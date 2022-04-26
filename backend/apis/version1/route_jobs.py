a0eb037cfe1a361b0074bf0b0d6426210e17b09b
werverton <weverton.si08@gmail.com>
2022-04-26 10:34:23 -0300

    add POST on route_jobs and tests

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,6 +1,8 @@
 from db.models.jobs import Job
 from db.repository.jobs import create_new_job
 from db.repository.jobs import retrieve_job
+from db.repository.jobs import list_jobs
+from db.repository.jobs import update_job_by_id
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
@@ -9,6 +11,7 @@ from fastapi import status
 from schemas.jobs import JobCreate
 from schemas.jobs import ShowJob
 from sqlalchemy.orm import Session
+from typing import List
 
 router = APIRouter()
 
@@ -26,3 +29,16 @@ def read_job(id:int,db:Session = Depends(get_db)):
     if not job:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Job with this id {id} does not exist")
     return job
+
+@router.get("/all",response_model=List[ShowJob]) #new
+def read_jobs(db:Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return jobs
+
+@router.put("/update/{id}")
+def update_job(id: int,job: JobCreate,db: Session = Depends(get_db)):
+    current_user = 1
+    message = update_job_by_id(id=id,job=job,db=db,owner_id=current_user)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Job with id {id} not found")
+    return {"msg":"Successfully updated data."}
--- backend/db/repository/jobs.py
+++ backend/db/repository/jobs.py
@@ -13,3 +13,16 @@ def create_new_job(job: JobCreate, db: Session, owner_id:int):
 def retrieve_job(id:int,db:Session):
     item = db.query(Job).filter(Job.id == id).first()
     return item
+    
+def list_jobs(db : Session):    #new
+    jobs = db.query(Job).filter(Job.is_active == True).all() #no exemplo do cara estava invertido filter com all e isso estava dando erro.
+    return jobs
+
+def update_job_by_id(id:int, job:JobCreate, db: Session,owner_id:int):
+    existing_job = db.query(Job).filter(Job.id == id)
+    if not existing_job.first():
+        return 0
+    job.__dict__.update(owner_id=owner_id)
+    existing_job.update(job.__dict__)
+    db.commit()
+    return 1
